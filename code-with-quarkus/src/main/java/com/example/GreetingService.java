af22697261742b5d8ce2270f0da5faea34f243cd
Sarmokadam <sneha.a.sarmokadam@capgemini.com>
2021-04-19 17:35:48 +0530

    sample app for quarkus

---
+++ code-with-quarkus/src/main/java/com/example/GreetingService.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+  public String greeting(String name) {
+
+    return "hello " + name;
+  }
+
+}
